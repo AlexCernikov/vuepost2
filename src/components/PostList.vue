ac5b12442ba9d6c0cace57eb48c3eac36ac2ae80
Alex <cernikov@mail.ru>
2022-07-12 21:44:57 +0300

    second commit

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="post" v-for="post in posts">
-  <div><strong>Title:</strong> {{post.title}}</div>
-  <div><strong>Description:</strong> {{post.body}}</div>
+<div>
+  <h3>List of Users</h3>
+   <post-item 
+   v-for="post in posts" 
+   :post="post"/>
 </div>
+
 </template>
 
 <script>
+import PostItem from '@/components/PostItem'
   export default {
+    components: {PostItem},
      props: {
        posts: {
          type: Array,
